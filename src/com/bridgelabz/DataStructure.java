ae4dd4725b126b6ae4bdeab5c02d7efe4d449acc
vanibm15 <vanibm15@gmail.com>
2022-05-10 10:47:10 +0530

    added:stack_push_method

---
+++ src/com/bridgelabz/DataStructure.java
@@ -0,0 +1,14 @@
+package com.bridgelabz;
+    public class DataStructure {
+        public static void main(String[] args) {
+            StackQueue<Integer> obj1 = new StackQueue();
+            obj1.push(70);
+            obj1.push(30);
+            obj1.push(56);
+            obj1.print();
+
+
+
+        }
+    }
+
